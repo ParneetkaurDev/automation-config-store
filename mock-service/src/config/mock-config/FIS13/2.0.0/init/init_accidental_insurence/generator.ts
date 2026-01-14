298f6f2bde1c6948102732828ea29732212e0425
“sourabhsaini11” <ss1841620@gmail.com>
2026-01-14 17:46:30 +0530

    fix:sachet bug fixes

--- mock-service/src/config/mock-config/FIS13/2.0.0/init/init_accidental_insurence/generator.ts
+++ mock-service/src/config/mock-config/FIS13/2.0.0/init/init_accidental_insurence/generator.ts
@@ -8,38 +8,6 @@ export async function initGenerator(existingPayload: any, sessionData: any) {
   if (sessionData.selected_fulfillments) {
     existingPayload.message.order.fulfillments = sessionData.fulfillments;
   }
-  if (sessionData.user_inputs) {
-    const nomineeInputs = sessionData.user_inputs;
-    const nomineeTag = existingPayload.message.order.fulfillments
-      .flatMap((f: any) => f.tags || [])
-      .find((t: any) => t.descriptor?.code === "NOMINEE_DETAILS");
-
-    nomineeTag.list.forEach((entry: any) => {
-      delete entry.descriptor?.short_desc
-      switch (entry.descriptor?.code) {
-        case "NOMINEE_NAME":
-          if (nomineeInputs.nominee_name)
-            entry.value = formatName(nomineeInputs.nominee_name);
-          break;
-        case "NOMINEE_DOB":
-          if (nomineeInputs.nominee_dob)
-            entry.value = ensureCountryCode(nomineeInputs.nominee_dob);
-          break;
-        case "APPOINTEE_NAME":
-          if (nomineeInputs.appointee_name) entry.value = nomineeInputs.appointee_name;
-          break;
-        case "APPOINTEE_RELATIONSHIP":
-          if (nomineeInputs.appointee_relation)
-            entry.value = nomineeInputs.appointee_relation;
-          break;
-        case "GOOD_HEALTH_DECLARATION":
-          if (nomineeInputs.health_declaration)
-            entry.value = nomineeInputs.health_declaration;
-          break;
-      }
-    });
-  }
-
   if (sessionData.selected_provider) {
     existingPayload.message.order.provider = sessionData.selected_provider;
   }
@@ -48,11 +16,4 @@ export async function initGenerator(existingPayload: any, sessionData: any) {
   return existingPayload;
 }
 
-function formatName(input: string): string {
-  return input.trim().split(/\s+/).join(" | ");
-}
 
-function ensureCountryCode(phoneNumber: string): string {
-  const trimmed = phoneNumber.trim();
-  return trimmed.startsWith("+91") ? trimmed : "+91-" + trimmed;
-}
--- mock-service/src/config/mock-config/FIS13/2.0.0/on_init/on_init_accidental_insurence/generator.ts
+++ mock-service/src/config/mock-config/FIS13/2.0.0/on_init/on_init_accidental_insurence/generator.ts
@@ -1,16 +1,27 @@
+function convertToISO(dateStr: any) {
+  const [dd, mm, yyyy] = dateStr.split("-");
+  return `${yyyy}-${mm}-${dd}`;
+}
+
 export async function onInitGenerator(existingPayload: any, sessionData: any) {
   let fulfillmentIds: string[] = [];
   let paymentIds: string[] = [];
   existingPayload.context.location.city.code = sessionData?.city_code;
-  const buyerGender = sessionData?.user_tags[0]?.list?.find(
+  const buyerGender = sessionData?.user_tags?.list?.find(
     (t: any) => t.descriptor?.code === "BUYER_GENDER"
   );
+  const buyerName = sessionData?.user_tags?.list?.find(
+    (t: any) => t.descriptor?.code === "BUYER_NAME"
+  );
+  const buyerDob = sessionData?.user_tags?.list?.find(
+    (t: any) => t.descriptor?.code === "BUYER_DOB"
+  );
 
-  const buyerEmail = sessionData?.user_tags[0]?.list?.find(
+  const buyerEmail = sessionData?.user_tags?.list?.find(
     (t: any) => t.descriptor?.code === "BUYER_EMAIL"
   );
 
-  const buyerPhone = sessionData?.user_tags[0]?.list?.find(
+  const buyerPhone = sessionData?.user_tags?.list?.find(
     (t: any) => t.descriptor?.code === "BUYER_PHONE_NUMBER"
   );
 
@@ -19,13 +30,13 @@ export async function onInitGenerator(existingPayload: any, sessionData: any) {
       fulfillmentIds.push(fulfillment.id);
       fulfillment.customer = {
         person: {
-          name: sessionData?.person_name || "John Doe",
-          dob: sessionData?.customer_dob || "2001-12-17",
-          gender: buyerGender || "M",
+          name: buyerName.value,
+          dob: convertToISO(buyerDob.value),
+          gender: buyerGender.value,
         },
         contact: {
-          phone: buyerPhone || "9090909090",
-          email: buyerEmail || "John.doe@example.com",
+          phone: buyerPhone.value,
+          email: buyerEmail.value,
         },
       };
     });
@@ -44,7 +55,7 @@ export async function onInitGenerator(existingPayload: any, sessionData: any) {
     existingPayload.message.order.payments.forEach(
       (payment: any, index: number) => {
         payment.id = `P${index}`;
-        payment.url = `https://fis.test.bpp.io/pg-gateway/payment?amount=1000&txn_id=${payment.id}`;
+        // payment.url = `https://fis.test.bpp.io/pg-gateway/payment?amount=1000&txn_id=${payment.id}`;
         paymentIds.push(payment.id);
       }
     );
