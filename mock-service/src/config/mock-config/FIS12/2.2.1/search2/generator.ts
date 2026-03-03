ee514569ecf812403db70d72694ef17937a8c047
ParneetkaurDev <parneet.kaur@ondc.org>
2026-03-03 13:01:52 +0530

    Revert "feat:working on 2.2.1 bug fixes"
    This reverts commit 236d67a0b165f1a8334743c41ce231cb424cd105.

--- mock-service/src/config/mock-config/FIS12/2.2.1/search2/generator.ts
+++ mock-service/src/config/mock-config/FIS12/2.2.1/search2/generator.ts
@@ -8,14 +8,21 @@ export async function searchDefaultGenerator(
   delete existingPayload.context.bpp_uri;
   delete existingPayload.context.bpp_id;
 
+  // Set start and end date dynamically
+  // const now = new Date();
+  // const end = new Date(now);
+  // end.setDate(now.getDate() + 2);
+  // if (
+  // 	existingPayload.message?.intent?.fulfillment?.stops?.[0]?.time?.range
+  // ) {
+  // 	existingPayload.message.intent.fulfillment.stops[0].time.range.start = now.toISOString();
+  // 	existingPayload.message.intent.fulfillment.stops[0].time.range.end = end.toISOString();
+  // }
+
   // Set city code from user inputs if available
   if (sessionData.user_inputs?.city_code) {
     existingPayload.context.location.city.code =
       sessionData.user_inputs.city_code;
-  }
-
-  if (sessionData.user_inputs && sessionData.user_inputs?.provider) {
-    existingPayload.message.intent.provider = sessionData.user_inputs?.provider
   }
 
   // Update form_response with status and submission_id (preserve existing structure)
